cac6d94297c94b986f92618ac6d7476a216451bd
TriUbi <triubilla@gmail.com>
2025-02-02 12:50:28 +0100

    Configurar GitHub Pages

--- client/src/services/authServices.ts
+++ client/src/services/authServices.ts
@@ -1,10 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 import { User } from "../models/User";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const authorizeUser = async (): Promise<false | User> => {
-    const response = await axios.get("http://localhost:3000/api/auth/authorize", {withCredentials: true})
-    if (response.status === 200) {
-        return response.data
-    } else return false
-}
+  const response = await axios.get(`${API_URL}/api/auth/authorize`, {
+    withCredentials: true,
+  });
+  if (response.status === 200) {
+    return response.data;
+  } else return false;
+};
