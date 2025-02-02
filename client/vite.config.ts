ba742af87c67b5d891f12a4eec9ae6dcea7e1ceb
TriUbi <triubilla@gmail.com>
2025-02-02 13:00:57 +0100

    Update GitHub Pages workflow

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Book-Webshop/", // Nombre de tu repositorio
+  base: process.env.NODE_ENV === "production" ? "/Book-Webshop/" : "/",
 });
